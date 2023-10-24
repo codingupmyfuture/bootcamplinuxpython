9aae52947700aef5c2363fe80f73e54e1351467c
luisvasv-teck <luis.vergara@teck.com>
2023-10-24 18:58:42 -0500

    sesion nro 5

---
+++ contenido.curso/003.notebooks/LVL-3/LV3_05_poo3/operaciones_matematicas/elementales.py
@@ -0,0 +1,23 @@
+def suma(numero1: int, numero2: int, demo: bool = False) -> int:
+    """esta función permite realizar la sima de dos numeros
+    los cuales son enteros, etc etc
+
+    :param numero1: número 1
+    :type numero1: int
+    :param numero2: número 2
+    :type numero2: int
+    :param demo: parámetro de prueba, defaults to False
+    :type demo: bool, optional
+    :return: la suma de los dos elementos
+    :rtype: int
+    """
+    return numero1 + numero2
+
+def resta(numero1: int, numero2: int) -> int:
+    return numero1 - numero2
+
+def multiplicacion(numero1: int, numero2: int) -> int:
+    return numero1 * numero2
+
+def division(numero1: int, numero2: int) -> float:
+    return numero1 / numero2
