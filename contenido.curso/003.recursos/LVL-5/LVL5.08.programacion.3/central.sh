34da6916d7620b8833af1525fde3920d0a2373ca
luisvasv-teck <luis.vergara@teck.com>
2024-02-23 12:02:02 -0500

    add programming session #8

---
+++ contenido.curso/003.recursos/LVL-5/LVL5.08.programacion.3/central.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# cargando el utilitario
+source ./utilitarios/genericos.sh
+
+
+encabezado_bootcamp_2
+
+imprimir_color $AZUL "valor para detectar errores: $CODIGO_ERROR_FUNCION"
+
+encabezado_bootcamp_3
